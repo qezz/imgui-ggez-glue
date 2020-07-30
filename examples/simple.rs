9fa7b74c4eabfbd57ec5de22779062c9a9b7877f
Sergey Mishin <sergei.a.mishin@gmail.com>
2020-07-30 09:53:37 +0300

    WIP

---
+++ examples/simple.rs
@@ -0,0 +1,141 @@
+extern crate ggez;
+
+use imgui_ggez_glue::wrapper::ImGuiWrapper;
+use ggez::conf;
+use ggez::event::{self, EventHandler, KeyCode, KeyMods, MouseButton};
+use ggez::graphics;
+use ggez::nalgebra as na;
+use ggez::{Context, GameResult};
+
+
+use imgui::*;
+use imgui_gfx_renderer::*;
+
+
+struct MainState {
+    pos_x: f32,
+    imgui_wrapper: ImGuiWrapper,
+    hidpi_factor: f32,
+}
+
+impl MainState {
+    fn new(mut ctx: &mut Context, hidpi_factor: f32) -> GameResult<MainState> {
+        let imgui_wrapper = ImGuiWrapper::new(&mut ctx);
+        let s = MainState {
+            pos_x: 0.0,
+            imgui_wrapper,
+            hidpi_factor,
+        };
+        Ok(s)
+    }
+}
+
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+
+        // Render game stuff
+        {
+            let circle = graphics::Mesh::new_circle(
+                ctx,
+                graphics::DrawMode::fill(),
+                na::Point2::new(self.pos_x, 380.0),
+                100.0,
+                2.0,
+                graphics::WHITE,
+            )?;
+            graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
+        }
+
+        // Render game ui
+        {
+            self.imgui_wrapper.render(ctx, self.hidpi_factor, Some(
+                Box::new(
+                    |ui: &imgui::Ui| {
+                        ui.text(im_str!("Hi from this label!"));
+                    }
+                ))
+            );
+        }
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+
+    fn mouse_motion_event(&mut self, _ctx: &mut Context, x: f32, y: f32, _dx: f32, _dy: f32) {
+        self.imgui_wrapper.update_mouse_pos(x, y);
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        button: MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+        self.imgui_wrapper.update_mouse_down((
+            button == MouseButton::Left,
+            button == MouseButton::Right,
+            button == MouseButton::Middle,
+        ));
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        _ctx: &mut Context,
+        _button: MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+        self.imgui_wrapper.update_mouse_down((false, false, false));
+    }
+
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: KeyCode,
+        keymods: KeyMods,
+        _repeat: bool,
+    ) {
+        self.imgui_wrapper.update_key_down(keycode, keymods);
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, keymods: KeyMods) {
+        self.imgui_wrapper.update_key_up(keycode, keymods);
+    }
+
+    fn text_input_event(&mut self, _ctx: &mut Context, val: char) {
+        self.imgui_wrapper.update_text(val);
+    }
+
+    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        graphics::set_screen_coordinates(ctx, graphics::Rect::new(0.0, 0.0, width, height))
+            .unwrap();
+        //println!("{:?}", graphics::screen_coordinates(ctx));
+    }
+
+    fn mouse_wheel_event(&mut self, _ctx: &mut Context, x: f32, y: f32) {
+        self.imgui_wrapper.update_scroll(x, y);
+    }
+}
+
+pub fn main() -> ggez::GameResult {
+    let cb = ggez::ContextBuilder::new("super_simple with imgui", "ggez")
+        .window_setup(conf::WindowSetup::default().title("super_simple with imgui"))
+        .window_mode(
+            conf::WindowMode::default().resizable(true), /*.dimensions(750.0, 500.0)*/
+        );
+    let (ref mut ctx, event_loop) = &mut cb.build()?;
+
+    let hidpi_factor = event_loop.get_primary_monitor().get_hidpi_factor() as f32;
+    println!("main hidpi_factor = {}", hidpi_factor);
+
+    let state = &mut MainState::new(ctx, hidpi_factor)?;
+
+    event::run(ctx, event_loop, state)
+}
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use imgui::Window;
+
+pub mod wrapper;
+
+// pub trait DrawWindow<'a> {
+//     fn window() -> Window<'a>;
+// }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn smoke() {
+        
+    }
+}
